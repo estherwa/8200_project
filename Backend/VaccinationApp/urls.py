772c2e3af4a8c1c3c5fbb2458cf92dbc713d695e
Esther Wahnon Amselem <82866031+estherwa@users.noreply.github.com>
2022-10-02 21:15:33 +0300

    docker

--- Backend/VaccinationApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import re_path as url
-
-from VaccinationApp import views
-
-urlpatterns = [
-    url(r'^patient$', views.patientApi),
-    url(r'^patient/(/[0-9]+)$', views.patientApi),
-]
--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.10
 
 WORKDIR /app
 
-COPY . /app
+COPY Frontend /app
 
 RUN  pip install -r requirements.txt
 
